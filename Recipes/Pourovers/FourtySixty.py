71d73936e49b4f358270552c50b36fc8673a6b53
ToCans <liverpool8914@gmail.com>
2020-12-30 23:30:57 -0500

    Strength Func, Small Edits

--- Recipes/Pourovers/FourtySixty.py
+++ Recipes/Pourovers/FourtySixty.py
@@ -3,12 +3,19 @@
 
 # Imports
 import time
+import os
 
 # Initializing
 user_check = False
+gram_check = False
 ratio_check = False
 
+
 # Definitions
+###############################
+def clear_terminal() :
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 def enter_to_cont():
     input("Press Enter to Continue...")
 
@@ -16,7 +23,7 @@ def next_step():
     print("\n****************************************************** \n")
 
 def profile(fourty_pour) :
-    question = "Please enter 1, 2, or 3 to determine your coffee's profile:\n\n 1. Bitter\n 2. Sweet\n 3. Balanced\n\nEnter here: "
+    question = "\nPlease enter 1, 2, or 3 to determine your coffee's profile:\n\n 1. Bitter\n 2. Balanced\n 3. Sweet\n\nEnter here: "
     prof_num = False
     while prof_num not in [1, 2, 3] :
         prof_num = input(question)
@@ -27,11 +34,11 @@ def profile(fourty_pour) :
                     bloom2 = fourty_pour * 0.4
                     break
             elif prof_num == 2 :
-                    bloom1 = fourty_pour * 0.4
-                    bloom2 = fourty_pour * 0.6
+                    bloom1 = bloom2 = fourty_pour * 0.5
                     break
             elif prof_num == 3 :
-                    bloom1 = bloom2 = fourty_pour * 0.5
+                    bloom1 = fourty_pour * 0.4
+                    bloom2 = fourty_pour * 0.6
                     break
             else :
                 print("\nError.")
@@ -39,18 +46,54 @@ def profile(fourty_pour) :
             print("\nError.")
     return bloom1,bloom2
    
-
-#def strength(str_num):
-
-print("The 40/60 Pourover Recipe")
+def strength(sixty_pour) :
+    question = "\nPlease enter 1, 2, or 3 to determine the strength of your coffee:\n\n 1. Weak\n 2. Balanced\n 3. Strong\n\nEnter here: "
+    str_num = False
+    while str_num not in [1, 2, 3] :
+        str_num = input(question)
+        try :
+            str_num = int(str_num)
+            if str_num == 1 :
+                    pour3 = sixty_pour / str_num
+                    pour4 = 0
+                    pour5 = 0
+                    break
+            elif str_num == 2 :
+                    pour3 = sixty_pour / str_num
+                    pour4 = sixty_pour / str_num
+                    pour5 = 0
+                    break
+            elif str_num == 3 :
+                    pour3 = sixty_pour / str_num
+                    pour4 = sixty_pour / str_num
+                    pour5 = sixty_pour / str_num
+                    break
+            else :
+                print("\nError.")
+        except :
+            print("\nError.")
+    return str_num, pour3, pour4, pour5
+###############################
 
 # Main Body
+clear_terminal()
+print("###########################\nThe 40/60 Pourover Recipe\n###########################\n")
 # Checks for # of Consumers
 while user_check == False:
     users = input("Please input the number of coffee consumers: ")
     try:
         users = int(users)
         user_check = True
+    except:
+        print("Error. Please enter a numerical value.\n")
+
+# Checks for the grams of coffee
+next_step()
+while gram_check == False:
+    grams = input("Please input the amount of coffee grams you will use per person: ")
+    try:
+        grams = float(grams)
+        gram_check = True
     except:
         print("Error. Please enter a numerical value.\n")
 
@@ -71,10 +114,14 @@ while ratio_check == False:
 
 # Calculaltes 40 and 60 percents 
 
-fourty_pour_init = float(users * ratio * 0.4)
-sixty_pour_init = float(users * ratio * 0.6)
+fourty_pour_init = float(users * grams * ratio * 0.4)
+sixty_pour_init = float(users * grams * ratio * 0.6)
+total_coffee_grams = float(users * grams)
 
-# Asks the user for their Coffee Profile Preference
+# Asks the user for their Coffee Profile and Strength Preferences
 next_step()
-print("Would you prefer the profile of your coffee to be bitter, sweet, or balanced?\n")
+print("Would you prefer the profile of your coffee to be bitter, balanced, or sweet?")
 pour1, pour2 = profile(fourty_pour_init)
+next_step()
+print("Would you prefer the strength of your coffee to be weak, balanced, or strong?")
+strength_num, third_pour, fourth_pour, fifth_pour = strength(sixty_pour_init)
