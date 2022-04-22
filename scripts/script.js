88888c08bb442b97092c8ad6e840d04114a713dd
ToCans <liverpool8914@gmail.com>
2022-04-22 16:34:55 -0400

    Simple If Statement Test

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,28 @@
 function offset_calcs() {
-    // Definitions of Main Menu
-    let basics_main_menu = document.getElementById('basics-tab').getBoundingClientRect();
-    let recipes_main_menu = document.getElementById('recipes-tab').getBoundingClientRect();
-    let equipment_main_menu = document.getElementById('equipment-tab').getBoundingClientRect();
-    let tools_main_menu = document.getElementById('tools-tab').getBoundingClientRect();
     if (window.matchMedia('(max-width: 768px)').matches) {
+        // Definitions of Main Menu
+        let basics_main_menu = document.getElementById('basics-tab');
+        let recipes_main_menu = document.getElementById('recipes-tab');
+        let equipment_main_menu = document.getElementById('equipment-tab');
+        let tools_main_menu = document.getElementById('tools-tab');
+
         // Definition of X Offsets
-        basic_menu_offset_x = -basics_main_menu.x + 'px';
-        recipes_menu_offset_x = -recipes_main_menu.x + 'px';
-        equipment_menu_offset_x = -equipment_main_menu.x + 'px';
-        tools_menu_offset_x = -tools_main_menu.x + 'px';
+        let basic_menu_offset_x = -basics_main_menu.getBoundingClientRect().x + 'px';
+        let recipes_menu_offset_x = -recipes_main_menu.getBoundingClientRect().x + 'px';
+        let equipment_menu_offset_x = -equipment_main_menu.getBoundingClientRect().x + 'px';
+        let tools_menu_offset_x = -tools_main_menu.getBoundingClientRect().x + 'px';
+
+        // Applying Offset
+        document.getElementById('basics-sub').style.marginLeft = basic_menu_offset_x;
+        document.getElementById('recipes-sub').style.marginLeft = recipes_menu_offset_x;
+        document.getElementById('equipment-sub').style.marginLeft = equipment_menu_offset_x;
+        document.getElementById('tools-sub').style.marginLeft = tools_menu_offset_x;
     } else {
-        basic_menu_offset_x = '-87.5px';
-        recipes_menu_offset_x = '-87.5px';
-        equipment_menu_offset_x = '-87.5px';
-        tools_menu_offset_x = '-87.5px';
+        document.getElementById('basics-sub').style.marginLeft = '-87.5px';
+        document.getElementById('recipes-sub').style.marginLeft = '-87.5px';
+        document.getElementById('equipment-sub').style.marginLeft = '-87.5px';
+        document.getElementById('tools-sub').style.marginLeft = '-87.5px';
     }
-    // Applying Offset
-    document.getElementById('basics-sub').style.marginLeft = basic_menu_offset_x;
-    document.getElementById('recipes-sub').style.marginLeft = recipes_menu_offset_x;
-    document.getElementById('equipment-sub').style.marginLeft = equipment_menu_offset_x;
-    document.getElementById('tools-sub').style.marginLeft = tools_menu_offset_x;
 }
 
 function image_scroller() {
